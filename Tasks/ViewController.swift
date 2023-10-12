6790971d5f929c26d9da53f09f48d32d2638f3fb
Markos da Silva <markosrsilva@gmail.com>
2023-10-12 18:39:53 -0300

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Markos Rodrigo Sousa da Silva on 12/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
