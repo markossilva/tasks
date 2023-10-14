96095fc87a55414bf628ebb7418aaf19dc052b09
Markos da Silva <markosrsilva@gmail.com>
2023-10-13 21:15:11 -0300

    create xcodegen

---
+++ Tasks/Modules/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Tasks
+//
+//  Created by Markos Rodrigo Sousa da Silva on 13/10/23.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    }
+}
